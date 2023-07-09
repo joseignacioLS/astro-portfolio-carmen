a7ca2dc6a2befd0d8a836f07a442c09d54737228
Jose <ls.joseignacio@gmail.com>
2023-07-09 22:09:27 +0200

    refactor gallery and subtitles

--- src/data/work.js
+++ src/data/work.js
@@ -2,24 +2,28 @@ export const work = [
   {
     id: "grupo-sala",
     title: "Grupo Sala",
+    subtitle: "Diseño Gráfico",
     mini: "/images/800_gruposala.png",
     labels: ["design"],
   },
   {
     id: "global-gap",
     title: "Global Gap",
+    subtitle: "Diseño Gráfico",
     mini: "/images/800_globalgap1-480x480.png",
     labels: ["design"],
   },
   {
     id: "small-genius",
     title: "Los Pequeños Genios",
+    subtitle: "Diseño Gráfico",
     mini: "/images/diverestiu_800x800-480x480.png",
     labels: ["design"],
   },
   {
     id: "diatoms",
     title: "Diatomeas",
+    subtitle: "Estampado",
     mini: "/images/Redes1-e1600257260863-480x480.png",
     labels: ["stamps"],
   },
