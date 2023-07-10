a6e0108d01319b86a63a99347a060dadcdf1c8a9
Jose <ls.joseignacio@gmail.com>
2023-07-10 14:13:19 +0200

    improved footer, added favicon, fixed gallery image width

---
+++ src/data/rrss.js
@@ -0,0 +1,18 @@
+export
+  const rrssData = [
+    {
+      title: "Instagram",
+      logo: "/logos/instagram-svgrepo-com.svg",
+      url: "https://instagram.com",
+    },
+    {
+      title: "Behance",
+      logo: "/logos/behance-svgrepo-com.svg",
+      url: "https://behance.com",
+    },
+    {
+      title: "Linkedin",
+      logo: "/logos/linkedin-svgrepo-com.svg",
+      url: "https://linkedin.com",
+    },
+  ];
